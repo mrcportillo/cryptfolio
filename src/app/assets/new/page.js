6cfe66bfe30a57ea9959c9faf3083bd39e14287b
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-22 16:28:03 -0300

    Add form validation and buttons to new asset page

--- src/app/assets/new/page.js
+++ src/app/assets/new/page.js
@@ -1,4 +1,6 @@
+import { create } from "@/app/actions/asset";
 import PageContainer from "@/app/components/PageContainer";
+import Buttons from "@/app/components/forms/Buttons";
 import Dropdown from "@/app/components/forms/Dropdown";
 import Input from "@/app/components/forms/Input";
 import list from "@/services/coin/list";
@@ -18,18 +20,11 @@ export default async function NewAsset() {
     <PageContainer>
       <div className="sm:w-full md:w-1/2">
         <h1 className="text-3xl">New Asset</h1>
-        <form className="my-4 flex flex-col">
-          <Input id="name" type="text" label="Alias" />
-          <Dropdown id="coin" label="Coin" options={coinOptions} />
-          <Input id="amount" type="number" label="Amount" />
-          <div className="ml-auto mt-3">
-            <button className=" w-40 rounded-md bg-red-800 p-2 text-white">
-              Cancel
-            </button>
-            <button className="ml-2 w-40 rounded-md bg-green-800 p-2 text-white">
-              Save
-            </button>
-          </div>
+        <form className="my-4 flex flex-col" action={create}>
+          <Input name="name" type="text" label="Alias" required/>
+          <Dropdown name="coin" label="Coin" options={coinOptions} />
+          <Input name="amount" type="d" label="Amount" required/>
+          <Buttons cancelLabel={"Cancel"} confirmLabel={"Save"} />
         </form>
       </div>
     </PageContainer>

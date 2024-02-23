772228637b88b79f318da874d3eb73b8a14f5d5a
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-23 16:48:40 -0300

    Refactor page.js to use new page components

--- src/app/assets/new/page.js
+++ src/app/assets/new/page.js
@@ -1,9 +1,12 @@
 import { create } from "@/app/actions/asset";
-import PageContainer from "@/app/components/PageContainer";
+import PageContainer from "@/app/components/pages/PageContainer";
 import Buttons from "@/app/components/forms/Buttons";
 import Dropdown from "@/app/components/forms/Dropdown";
 import Input from "@/app/components/forms/Input";
 import list from "@/services/coin/list";
+import PageHeaeder from "@/app/components/pages/PageHeader";
+import PageContent from "@/app/components/pages/PageContent";
+import PageTitle from "@/app/components/pages/PageTitle";
 
 async function getCoinOptions() {
   const coinList = await list(50);
@@ -19,13 +22,17 @@ export default async function NewAsset() {
   return (
     <PageContainer>
       <div className="sm:w-full md:w-1/2">
-        <h1 className="text-3xl">New Asset</h1>
-        <form className="my-4 flex flex-col" action={create}>
-          <Input name="name" type="text" label="Alias" required/>
-          <Dropdown name="coin" label="Coin" options={coinOptions} />
-          <Input name="amount" type="d" label="Amount" required/>
-          <Buttons cancelLabel={"Cancel"} confirmLabel={"Save"} />
-        </form>
+        <PageHeaeder>
+          <PageTitle>New Asset</PageTitle>
+        </PageHeaeder>
+        <PageContent>
+          <form className="flex flex-col" action={create}>
+            <Input name="name" type="text" label="Alias" required />
+            <Dropdown name="coin" label="Coin" options={coinOptions} />
+            <Input name="amount" type="d" label="Amount" required />
+            <Buttons cancelLabel={"Cancel"} confirmLabel={"Save"} />
+          </form>
+        </PageContent>
       </div>
     </PageContainer>
   );

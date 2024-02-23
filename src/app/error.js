a20808b4796eaea63dcc46b6e78e3d2ee11e4519
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-23 16:49:13 -0300

    Update import path for PageContainer component

--- src/app/error.js
+++ src/app/error.js
@@ -1,7 +1,7 @@
 "use client"; // Error components must be Client Components
 
 import { useEffect } from "react";
-import PageContainer from "./components/PageContainer";
+import PageContainer from "./components/pages/PageContainer";
 
 export default function Error({ error, reset }) {
   useEffect(() => {

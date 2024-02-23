3d7319e4631a9dc0e73156c3c3a0ddd81b2af39d
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-23 16:47:38 -0300

    Refactor page.js to use new components

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,6 +1,9 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import Image from "next/image";
-import PageContainer from "../components/PageContainer";
+import PageContainer from "../components/pages/PageContainer";
+import PageHeaeder from "../components/pages/PageHeader";
+import PageContent from "../components/pages/PageContent";
+import PageTitle from "../components/pages/PageTitle";
 
 export default async function Profile() {
   const { user } = await getSession();
@@ -8,13 +11,21 @@ export default async function Profile() {
   return (
     user && (
       <PageContainer>
-        <Image src={user.picture} alt={user.name} width={100} height={100} />
-        <div className="mt-2">
-          <h2>{user.name}</h2>
-          <a href="/api/auth/logout" className="text-red-800 hover:text-red-600">
-            Logout
-          </a>
-        </div>
+        <PageHeaeder>
+          <PageTitle>Profile</PageTitle>
+        </PageHeaeder>
+        <PageContent>
+          <Image src={user.picture} alt={user.name} width={100} height={100} />
+          <div className="mt-2">
+            <h2>{user.name}</h2>
+            <a
+              href="/api/auth/logout"
+              className="text-red-800 hover:text-red-600"
+            >
+              Logout
+            </a>
+          </div>
+        </PageContent>
       </PageContainer>
     )
   );

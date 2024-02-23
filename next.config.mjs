5dc8f6e6be84034e15d4f379084dc4585d3f3f01
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-23 19:20:23 -0300

    Add additional CDN for images

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "s.gravatar.com",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
     ],
   },
 };

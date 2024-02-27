a661f2bbce08d56c0b7945cd8b2887e46d3a86e1
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-27 16:13:32 -0300

    added SpeedInsights component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "../components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <UserProvider>
         <body className={inter.className}>
+          <SpeedInsights />
           <NavBar />
           {children}
         </body>

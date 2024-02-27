0de480f17b381e64f60151f7053dab531a05ad13
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-27 13:42:41 -0300

    Refactor AssetPill component to use Next.js routing

--- src/components/AssetPill/index.js
+++ src/components/AssetPill/index.js
@@ -1,31 +1,44 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const AssetName = ({ name }) => (
-  <h1 className="text-primary-900 mb-4 text-xl font-semibold">{name}</h1>
+  <h1 className="mb-4 text-xl font-semibold text-primary-900">{name}</h1>
 );
 
 const AssetAttribute = ({ label, value }) => (
   <div className="flex">
-    <div className="text-primary-800 mr-2">{label}</div>
+    <div className="mr-2 text-primary-800">{label}</div>
     <div className="text-primary-50">{value}</div>
   </div>
 );
 
 export default function AssetPill({ asset }) {
+  const router = useRouter();
+
+  const onEditNavigate = (event) => {
+    event.preventDefault();
+    router.push(`/assets/${asset.id}/edit`);
+  };
+
   return (
-    <div className="from-primary-300  to-primary-700 rounded-md bg-gradient-to-br p-4 shadow">
-      <div className="flex justify-between">
-        <AssetName name={asset.assetName} />
-        <a href={`/assets/${asset.id}`} className="text-primary-200">
-          Edit
-        </a>
-      </div>
-      <AssetAttribute label="Coin" value={asset.assetId} />
-      <AssetAttribute label="Amount" value={asset.amount} />
-      <AssetAttribute label="Asset value" value={asset.price} />
-      <AssetAttribute label="$" value={asset.amount * asset.price} />
-      <AssetAttribute
-        label="Last updated"
-        value={new Date(asset.date).toDateString()}
-      />
+    <div className="cursor-pointer  rounded-md bg-gradient-to-br from-primary-300 to-primary-700 p-4 shadow">
+      <Link href={`/assets/${asset.id}`}>
+        <div className="flex justify-between">
+          <AssetName name={asset.assetName} />
+          <div className="text-primary-200" onClick={onEditNavigate}>
+            Edit
+          </div>
+        </div>
+        <AssetAttribute label="Coin" value={asset.assetId} />
+        <AssetAttribute label="Amount" value={asset.amount} />
+        <AssetAttribute label="Asset value" value={asset.price} />
+        <AssetAttribute label="$" value={asset.amount * asset.price} />
+        <AssetAttribute
+          label="Last updated"
+          value={new Date(asset.date).toDateString()}
+        />
+      </Link>
     </div>
   );
 }

20f4c9215d6748c9fb99ea2cb0900682ca1be938
mrcportillo <mrcportillo@users.noreply.github.com>
2024-02-28 13:12:01 -0300

    added home loading

---
+++ src/app/home/loading.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import PageContainer from "../../components/pages/PageContainer";
+import PageHeaeder from "../../components/pages/PageHeader";
+import PageContent from "../../components/pages/PageContent";
+import Button from "../../components/Button";
+import PageTitle from "../../components/pages/PageTitle";
+
+export default function LoadingHome() {
+  return (
+    <PageContainer>
+      <PageHeaeder>
+        <PageTitle>Assets</PageTitle>
+        <div className="ml-auto sm:ml-8">
+          <Link href="/assets/new">
+            <Button>New asset</Button>
+          </Link>
+        </div>
+      </PageHeaeder>
+      <PageContent>
+        <div>loading assets..</div>
+      </PageContent>
+    </PageContainer>
+  );
+}

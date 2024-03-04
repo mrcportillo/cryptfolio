df9a4b8b4d46f83dfb9be406f99f7ae884afe891
mrcportillo <mrcportillo@users.noreply.github.com>
2024-03-04 16:56:52 -0300

    Refactor imports and add UserPortfolioValue component

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import PageContainer from "../../components/pages/PageContainer";
-import PageHeaeder from "../../components/pages/PageHeader";
-import PageContent from "../../components/pages/PageContent";
-import UserAssetsList from "../../components/UserAssetsList";
-import Button from "../../components/Button";
-import PageTitle from "../../components/pages/PageTitle";
+import PageContainer from "@/components/pages/PageContainer";
+import PageHeaeder from "@/components/pages/PageHeader";
+import PageContent from "@/components/pages/PageContent";
+import UserAssetsList from "@/components/UserAssetsList";
+import Button from "@/components/Button";
+import PageTitle from "@/components/pages/PageTitle";
+import UserPortfolioValue from "@/components/UserPortfolioValue";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       </PageHeaeder>
       <PageContent>
         <UserAssetsList />
+        <UserPortfolioValue />
       </PageContent>
     </PageContainer>
   );

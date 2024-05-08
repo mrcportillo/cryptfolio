cec62b646a4aad44ed21cd8cba6c95ae13073985
mrcportillo <mrcportillo@users.noreply.github.com>
2024-05-08 17:35:08 -0300

    chore: update middleware.js to exclude Azure static web app paths

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,9 @@
-import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
-
-export default withMiddlewareAuthRequired();
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - .swa (Azure static web apps)
+       */
+      '/((?!.swa).*)',
+    ],
+  }

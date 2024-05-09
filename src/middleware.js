cc53c68ac6de52025f972e9f893a0bbddd8c10b3
mrcportillo <mrcportillo@users.noreply.github.com>
2024-05-09 15:16:03 -0300

    Revert "chore: update middleware.js to exclude Azure static web app paths"
    This reverts commit cec62b646a4aad44ed21cd8cba6c95ae13073985.

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,3 @@
-export const config = {
-    matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - .swa (Azure static web apps)
-       */
-      '/((?!.swa).*)',
-    ],
-  }
+import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+
+export default withMiddlewareAuthRequired();
